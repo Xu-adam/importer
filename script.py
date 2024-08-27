95cb35a2872afdc13b946aa2c7734fe1ebad0920
Xu <xupeiyu0827@163.com>
2024-08-27 09:34:39 +0800

    Add files via upload

--- script.py
+++ script.py
@@ -6,8 +6,9 @@ import time
 from queue import Queue
 import gc
 import psutil
+import math
 
-### function definition
+### function definitions ###
 
 MEMORY_THRESHOLD = 5 * 1024 * 1024 * 1024 # 5GB
 def check_memory():
@@ -37,6 +38,7 @@ def insert_csv_to_db(chunk, connection_queue, table_name):
         if 'conn' in locals():
             connection_queue.put(conn)
 
+# submit the next percentage of the chunks to the thread pool
 def submit_next_percentage(executor, chunk_iter, submit_size, connection_queue, table):
     futures = {}
     try:
@@ -50,7 +52,7 @@ def submit_next_percentage(executor, chunk_iter, submit_size, connection_queue, 
 ###########################################################
 
 def main():
-    # ask for the configuration
+    # ask for the database configuration
     server = input("Please enter the server name: ")
     database = input("Please enter the database name: ")
     table = input("please enter the table name: ")
@@ -58,13 +60,13 @@ def main():
     password = input("Please enter the password for sa: ")
     connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};MARS_Connection=Yes;MaxPoolSize=10'
 
-    # get all the csv files in the current directory
+    # get the csv files to import in the current directory
     script_directory = os.getcwd()
     files = [os.path.join(script_directory, f) for f in os.listdir(script_directory) if f.endswith('.csv') and os.path.isfile(os.path.join(script_directory, f))]
     print(f"{len(files)} target csv files found in the current directory.")
 
     # multi-threading setup and database connection
-    max_workers = max_connections = batch_size = 5
+    max_workers = max_connections = 5
     split_size = 50000
     connection_queue = Queue(maxsize=max_connections)
 
@@ -75,7 +77,7 @@ def main():
 
     # iterate over all the csv files to import
     for input_file in files:
-        # split the csv file into chunks
+        # read and split the csv file into chunks
         start_time_for_split = time.time()
         print(f"Reading and Splitting {input_file} into chunks...")
         chunk_iter = pd.read_csv(input_file, chunksize=split_size, encoding='utf-8', low_memory=False, keep_default_na=False)
@@ -83,49 +85,54 @@ def main():
         chunk_iter = pd.read_csv(input_file, chunksize=split_size, encoding='utf-8', low_memory=False, keep_default_na=False)
         print(f"Read and Split {input_file} into {chunk_num} chunks in {(time.time() - start_time_for_split) / 60:.2f} minutes.")
         del start_time_for_split
-        total_batch_num = chunk_num / batch_size
         check_memory()
 
         # start timer
-        print(f"Start importing {input_file} to the database ...")
         total_start_time = time.time()
+        print(f"Start importing {input_file} to the database ...")
+        # submit 20% at a time to save memory
+        submit_size = math.ceil(chunk_num / 5)
+        percent_checkpoint = math.ceil(chunk_num / 10)
+
         # begin multi-threading
-        submit_size = chunk_num // 5
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
             # use a dictionary to store the future objects
             futures = submit_next_percentage(executor, chunk_iter, submit_size, connection_queue, table)
             rest_chunk_num -= submit_size
             check_memory()
             completed_files = 0
-            batch_number = 1
-            start_time_for_batch = time.time()
+            completed_percentage = 0
 
+            # make sure that all the chunks are processed
             while completed_files < chunk_num:
                 for future in as_completed(futures):
                     del futures[future]
                     completed_files += 1
-                    if completed_files % batch_size == 0:
-                        print(f"Batch {batch_number}/{total_batch_num}: {(time.time() - start_time_for_batch) / 60:.2f} minutes.")
-                        start_time_for_batch = time.time()
-                        batch_number += 1
+
+                    if completed_files % percent_checkpoint == 0:
+                        completed_percentage += 10
+                        print(f"{completed_percentage}% completed in {(time.time() - total_start_time) / 60:.2f} minutes.")
+
                     # check every 20% of the chunks
                     if completed_files % submit_size  == 0:
-                        clean()
+                        clean() # free memory of the previous 20%
                         submit_size = min(submit_size, rest_chunk_num)
                         if submit_size > 0:
+                            # submit the next 20% (or the rest)
                             new_futures = submit_next_percentage(executor, chunk_iter, submit_size, connection_queue, table)
                             rest_chunk_num -= submit_size
                             futures.update(new_futures)
                             check_memory()
                         else:
+                            # no more chunks to submit
                             break
-
 
         print(f"Data import completed in {(time.time() - total_start_time) / 60:.2f} minutes.")
         print(f"{input_file} has been imported to the database.")
         print("Cleaning up and moving on to the next file...")
         clean()
 
+    # all files have been processed, close the database connection
     while not connection_queue.empty():
         conn = connection_queue.get()
         conn.close()
