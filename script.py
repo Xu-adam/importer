cc5808755e69b7069c6611af7bbb00017055ef71
Xu <xupeiyu0827@163.com>
2024-08-30 08:49:09 +0800

    Add files via upload

--- script.py
+++ script.py
@@ -10,6 +10,14 @@ import math
 
 ### function definitions ###
 
+def partition():
+    line = "-" * 100
+    print(line)
+
+def end_file():
+    partition()
+    partition()
+
 MEMORY_THRESHOLD = 5 * 1024 * 1024 * 1024 # 5GB
 def check_memory():
     available_memory = psutil.virtual_memory().available
@@ -53,11 +61,14 @@ def submit_next_percentage(executor, chunk_iter, submit_size, connection_queue, 
 
 def main():
     # ask for the database configuration
-    server = input("Please enter the server name: ")
-    database = input("Please enter the database name: ")
-    table = input("please enter the table name: ")
-    username = input("Please enter the username(input sa): ")
-    password = input("Please enter the password for sa: ")
+    partition()
+    server = input("The server name: ")
+    database = input("The database name: ")
+    table = input("The table name: ")
+    username = input("The username (sa recommended): ")
+    password = input("The password: ")
+    max_workers = int(input("Number of threads to use (5 recommended): "))
+    partition()
     connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};MARS_Connection=Yes;MaxPoolSize=10'
 
     # get the csv files to import in the current directory
@@ -66,33 +77,35 @@ def main():
     print(f"{len(files)} target csv files found in the current directory.")
 
     # multi-threading setup and database connection
-    max_workers = max_connections = 5
+    max_connections = max_workers
     split_size = 50000
     connection_queue = Queue(maxsize=max_connections)
 
     for _ in range(max_connections):
         conn = pyodbc.connect(connection_string)
         connection_queue.put(conn)
-    print('Database connection established.')
+    print(f"{max_connections} database connections established. {max_workers} threads will be used.")
+    partition()
 
     # iterate over all the csv files to import
     for input_file in files:
         # read and split the csv file into chunks
         start_time_for_split = time.time()
-        print(f"Reading and Splitting {input_file} into chunks...")
+        print(f"Reading and Splitting {input_file}...")
         chunk_iter = pd.read_csv(input_file, chunksize=split_size, encoding='utf-8', low_memory=False, keep_default_na=False)
         rest_chunk_num = chunk_num = sum(1 for _ in chunk_iter)
         chunk_iter = pd.read_csv(input_file, chunksize=split_size, encoding='utf-8', low_memory=False, keep_default_na=False)
-        print(f"Read and Split {input_file} into {chunk_num} chunks in {(time.time() - start_time_for_split) / 60:.2f} minutes.")
+        print(f"Read and Split into {chunk_num} chunks (each with {split_size} rows) in {(time.time() - start_time_for_split) / 60:.2f} minutes.")
         del start_time_for_split
         check_memory()
+        partition()
 
         # start timer
         total_start_time = time.time()
-        print(f"Start importing {input_file} to the database ...")
+        print(f"Start importing {input_file} to database ...")
         # submit 20% at a time to save memory
         submit_size = math.ceil(chunk_num / 5)
-        percent_checkpoint = math.ceil(chunk_num / 10)
+        percent_checkpoint = math.ceil(chunk_num / 20)
 
         # begin multi-threading
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
@@ -100,6 +113,7 @@ def main():
             futures = submit_next_percentage(executor, chunk_iter, submit_size, connection_queue, table)
             rest_chunk_num -= submit_size
             check_memory()
+            partition()
             completed_files = 0
             completed_percentage = 0
 
@@ -110,7 +124,7 @@ def main():
                     completed_files += 1
 
                     if completed_files % percent_checkpoint == 0:
-                        completed_percentage += 10
+                        completed_percentage += 5
                         print(f"{completed_percentage}% completed in {(time.time() - total_start_time) / 60:.2f} minutes.")
 
                     if submit_size > 0:
@@ -124,10 +138,12 @@ def main():
                                 rest_chunk_num -= submit_size
                                 futures.update(new_futures)
                                 check_memory()
+                                partition()
 
         print(f"Data import completed in {(time.time() - total_start_time) / 60:.2f} minutes.")
         print(f"{input_file} has been imported to the database.")
         print("Cleaning up and moving on to the next file...")
+        end_file()
         clean()
 
     # all files have been processed, close the database connection
